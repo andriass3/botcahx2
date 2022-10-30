727f486197e2ee98dddd0b2e569b513ba8542acd
andriass3 <116412292+andriass3@users.noreply.github.com>
2022-10-30 19:39:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6282221792667', 'TioXd', true]
+  ['6282288082120', 'Andrias', true]
 // [owner bukan bot!]
 ] // paste nomor di sini
-global.mods = ['6282221792667'] // bantuan?
-global.prems = ['6282221792667'] // user premium limit tanpa batas 
+global.mods = ['6282288082120'] // bantuan?
+global.prems = ['6282288082120'] // user premium limit tanpa batas 
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrttm: 'https://nurutomo.herokuapp.com',
@@ -22,9 +22,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Wm untuk stiker dan autobio
-global.tio = 'TioXd'
-global.packname = '© BOTCAHX'
-global.author = 'TioXd'
+global.tio = 'YazBot'
+global.packname = '© ANDRIAS SETIAWAN SUBARDO'
+global.author = 'YazBot'
 
 global.multiplier = 70 // Semakin Tinggi levelmu semakin naik
 
